e6868e193d53b0bfabe0a339ea5f68428924e650
sam byers <samwbyers@gmail.com>
2017-03-13 15:58:56 -0400

    fixed the provisioning script for vagrant box

--- prov_script.sh
+++ prov_script.sh
@@ -1,5 +1,6 @@
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install python-pip -y
 sudo pip install --upgrade pip
-sudo pip install tweetpy
+sudo pip install tweepy
+git clone https://github.com/sambyers/tweetled /home/ubuntu/tweetled
