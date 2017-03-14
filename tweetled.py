91c73b36b14403dc69cd031972e2a3393e5df01a
sam byers <samwbyers@gmail.com>
2017-03-13 21:33:39 -0400

    test tweepy

--- tweetled.py
+++ tweetled.py
@@ -6,4 +6,28 @@ import yaml
 # Load config file
 cfg_file_path = file("/vagrant/tweetled_config.yaml", "r")
 cfg_file = yaml.load(cfg_file_path)
-print cfg_file
+
+consumer_key = cfg_file['consumer_key']
+consumer_secret = cfg_file['consumer_secret']
+
+access_token = cfg_file['access_token']
+access_token_secret = cfg_file['access_token_secret']
+
+class StdOutListener(StreamListener):
+    """ A listener handles tweets that are received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+if __name__ == '__main__':
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, l)
+    stream.filter(track=['vcu'])
