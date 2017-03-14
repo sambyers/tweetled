81bba8ff527c8d593137b5be2c582e3462ad19a2
sam byers <samwbyers@gmail.com>
2017-03-14 11:17:43 -0400

    check for cfg file in two places

--- tweetled.py
+++ tweetled.py
@@ -4,8 +4,15 @@ from tweepy import Stream
 import yaml
 
 # Load config file
-cfg_file_path = file("/vagrant/tweetled_config.yaml", "r")
-cfg_file = yaml.load(cfg_file_path)
+if file("/vagrant/tweetled_config.yaml", "r"):
+	cfg_file_path = file("/vagrant/tweetled_config.yaml", "r")
+elif file("./tweetled_config.yaml", "r"):
+	cfg_file_path = file("./tweetled_config.yaml", "r")
+	
+if cfg_file:
+	cfg_file = yaml.load(cfg_file_path)
+else:
+	print "No config file found."
 
 consumer_key = cfg_file['consumer_key']
 consumer_secret = cfg_file['consumer_secret']
@@ -13,10 +20,8 @@ consumer_secret = cfg_file['consumer_secret']
 access_token = cfg_file['access_token']
 access_token_secret = cfg_file['access_token_secret']
 
-class StdOutListener(StreamListener):
-    """ A listener handles tweets that are received from the stream.
-    This is a basic listener that just prints received tweets to stdout.
-    """
+class listener(StreamListener):
+
     def on_data(self, data):
         print(data)
         return True
@@ -25,9 +30,12 @@ class StdOutListener(StreamListener):
         print(status)
 
 if __name__ == '__main__':
-    l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    l = listener()
+    try:
+	    auth = OAuthHandler(consumer_key, consumer_secret)
+	    auth.set_access_token(access_token, access_token_secret)
+	except tweepy.TweepError:
+		print "Authentication failed."
 
     stream = Stream(auth, l)
-    stream.filter(track=['devnet roanoke'])
+    stream.filter(track=['#devnetroanoke'])
