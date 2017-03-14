e04e865699b776ee6fc2f7ea93151adb8c188632
sam byers <samwbyers@gmail.com>
2017-03-13 20:31:41 -0400

    early commit

--- tweetled.py
+++ tweetled.py
@@ -1,1 +1,11 @@
-from tweepy import 
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+consumer_key="djoP47VjZyXX5ddC29uNZFEUO"
+consumer_secret="nod9LaQalsov4i4EAE85LflX2bhITQCiKF4U9W61cNghPNZU2N"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+access_token=""
+access_token_secret=""
