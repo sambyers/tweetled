5b8497739e984d671e85b518cb32c0d18f39e3e7
sam byers <samwbyers@gmail.com>
2017-03-21 12:15:49 -0400

    added more code from led matrix

--- tweetled.py
+++ tweetled.py
@@ -33,93 +33,43 @@ def random_color():
         color = random.randrange(0, 255)
         return color
 
-class matrix_base(object):
-    def __init__(self, *args, **kwargs):
-        self.parser = argparse.ArgumentParser()
 
-        self.parser.add_argument("-r", "--led-rows", action="store", help="Display rows. 16 for 16x32, 32 for 32x32. Default: 32", default=32, type=int)
-        self.parser.add_argument("-c", "--led-chain", action="store", help="Daisy-chained boards. Default: 1.", default=1, type=int)
-        self.parser.add_argument("-P", "--led-parallel", action="store", help="For Plus-models or RPi2: parallel chains. 1..3. Default: 1", default=1, type=int)
-        self.parser.add_argument("-p", "--led-pwm-bits", action="store", help="Bits used for PWM. Something between 1..11. Default: 11", default=11, type=int)
-        self.parser.add_argument("-b", "--led-brightness", action="store", help="Sets brightness level. Default: 100. Range: 1..100", default=100, type=int)
-        self.parser.add_argument("-m", "--led-gpio-mapping", help="Hardware Mapping: regular, adafruit-hat, adafruit-hat-pwm" , choices=['regular', 'adafruit-hat', 'adafruit-hat-pwm'], type=str)
-        self.parser.add_argument("--led-scan-mode", action="store", help="Progressive or interlaced scan. 0 Progressive, 1 Interlaced (default)", default=1, choices=range(2), type=int)
-        self.parser.add_argument("--led-pwm-lsb-nanoseconds", action="store", help="Base time-unit for the on-time in the lowest significant bit in nanoseconds. Default: 130", default=130, type=int)
-        self.parser.add_argument("--led-show-refresh", action="store_true", help="Shows the current refresh rate of the LED panel")
-        self.parser.add_argument("--led-slowdown-gpio", action="store", help="Slow down writing to GPIO. Range: 1..100. Default: 1", choices=range(3), type=int)
-        self.parser.add_argument("--led-no-hardware-pulse", action="store", help="Don't use hardware pin-pulse generation")
+def run_led_text(my_text):
 
+    # Configuration for the matrix
+    options = RGBMatrixOptions()
+    options.rows = 16
+    options.chain_length = 1
+    options.parallel = 1
+    options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
 
-    def usleep(self, value):
-        time.sleep(value / 1000000.0)
+    matrix = RGBMatrix(options = options)
 
-    def run(self):
-        print("Running")
+    offscreen_canvas = matrix.CreateFrameCanvas()
+    font = graphics.Font()
+    font.LoadFont("/fonts/7x13.bdf")
+    textColor = graphics.Color(random_color(), random_color(), random_color())
+    pos = offscreen_canvas.width
+    print my_text
 
-    def process(self):
-        self.args = self.parser.parse_args()
+    while True:
+        offscreen_canvas.Clear()
+        len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
+        pos -= 1
+        if (pos + len < 0):
+            pos = offscreen_canvas.width
 
-        options = RGBMatrixOptions()
-
-        if self.args.led_gpio_mapping != None:
-          options.hardware_mapping = self.args.led_gpio_mapping
-        options.rows = self.args.led_rows
-        options.chain_length = self.args.led_chain
-        options.parallel = self.args.led_parallel
-        options.pwm_bits = self.args.led_pwm_bits
-        options.brightness = self.args.led_brightness
-        options.pwm_lsb_nanoseconds = self.args.led_pwm_lsb_nanoseconds
-        if self.args.led_show_refresh:
-          options.show_refresh_rate = 1
-
-        if self.args.led_slowdown_gpio != None:
-            options.gpio_slowdown = self.args.led_slowdown_gpio
-        if self.args.led_no_hardware_pulse:
-          options.disable_hardware_pulsing = True
-
-        self.matrix = RGBMatrix(options = options)
-
-        try:
-            # Start loop
-            print("Press CTRL-C to stop sample")
-            self.run()
-        except KeyboardInterrupt:
-            print("Exiting\n")
-            sys.exit(0)
-
-        return True
-
-class RunText(matrix_base):
-    def __init__(self, *args, **kwargs):
-        super(RunText, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
-
-    def run(self):
-        offscreen_canvas = self.matrix.CreateFrameCanvas()
-        font = graphics.Font()
-        font.LoadFont("/fonts/7x13.bdf")
-        textColor = graphics.Color(random_color(), random_color(), random_color())
-        pos = offscreen_canvas.width
-        my_text = self.args.text
-        print my_text
-
-        while True:
-            offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
-
-            time.sleep(0.05)
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+        time.sleep(0.05)
+        offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
 
 class listener(StreamListener):
     def on_data(self, data):
         data = json.loads(data)
         text = data['text']
         screen_name = data['user']['screen_name']
-        print screen_name + " tweeted: " + text
-        run_text = RunText(my_text=screen_name + " tweeted: " + text)
+        msg = screen_name + " tweeted: " + text
+        print msg
+        run_text = run_led_text(msg)
         return True
 
     def on_error(self, status):
