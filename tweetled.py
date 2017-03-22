5150b742e80b8cbf02cc09d512ee32ddbb338ad7
sam byers <samwbyers@gmail.com>
2017-03-21 23:32:00 -0400

    this got really hacky...

--- tweetled.py
+++ tweetled.py
@@ -85,8 +85,6 @@ def run_led_text(my_text, matrix):
         time.sleep(0.05)
         offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
 
-    offscreen_canvas = matrix.CreateFrameCanvas()
-
 class listener(StreamListener):
 
     def on_status(self, status):
